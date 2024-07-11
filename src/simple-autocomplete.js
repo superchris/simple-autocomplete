8ac1d4c91a9529884b104639353853e24c06d946
Chris Nelson <superchrisnelson@gmail.com>
2024-07-11 14:56:26 -0400

    some improvement

--- src/simple-autocomplete.js
+++ src/simple-autocomplete.js
@@ -6,24 +6,39 @@ export class SimpleAutocomplete extends HTMLElement {
     super();
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.innerHTML = `
-    <slot name="input"></slot>
+    <input name=${this.getAttribute('name')}>
     <slot name="list"></slot>
     `;
-    const inputSlot = this.shadowRoot.querySelector('slot[name="input"]');
-    const input = inputSlot.assignedElements().length > 0 ? inputSlot.assignedElements()[0] : undefined;
     const debounceMs = this.getAttribute('debounce') ? parseInt(this.getAttribute('debounce')) : 300;
-    input.addEventListener('input',
+    this.searchInput.addEventListener('input',
       debounce((e) => this.dispatchEvent(
-        new CustomEvent('autocomplete-search', { detail: { query: input.value } })), debounceMs));
-    const listSlot = this.shadowRoot.querySelector('slot[name="list"]');
-    const list = listSlot.assignedElements().length > 0 ? listSlot.assignedElements()[0] : undefined;
-    const combobox = new Combobox(input, list)
-    // when options appear, start intercepting keyboard events for navigation
-    combobox.start();
-    this.addEventListener("combobox-commit", ({detail, target}) => {
-      if (this.getAttribute('clear-on-select')) {input.value = '';}
-      target.dispatchEvent(new CustomEvent('autocomplete-commit', {detail, bubbles: true}));
+        new CustomEvent('autocomplete-search', { detail: { query: this.searchInput.value } })), debounceMs));
+    this.list.addEventListener("combobox-commit", ({detail, target}) => {
+      if (this.getAttribute('clear-on-select')) {this.searchInput.value = '';}
+      this.dispatchEvent(new CustomEvent('autocomplete-commit', {detail: target.dataset, bubbles: true}));
     })
+  }
+
+  get list() {
+    if (this.getAttribute('list')) {
+      return document.querySelector(`#${this.getAttribute('list')}`);
+    }
+    const listSlot = this.shadowRoot.querySelector('slot[name="list"]');
+    return listSlot.assignedElements().length > 0 ? listSlot.assignedElements()[0] : undefined;
+  }
+
+  initializeComboBox() {
+    this.combobox = new Combobox(this.searchInput, this.list)
+    // when options appear, start intercepting keyboard events for navigation
+    this.combobox.start();
+  }
+
+  connectedCallback() {
+    this.initializeComboBox();
+  }
+
+  get searchInput() {
+    return this.shadowRoot.querySelector('input');
   }
 }
 
